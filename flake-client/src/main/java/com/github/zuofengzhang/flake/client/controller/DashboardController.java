68ad709cba4368a0037f6250f6a076a163e90764
averyzhang <averyzhang@tencent.com>
2020-12-20 01:01:25 +0800

    dashboard ui

--- flake-client/src/main/java/com/github/zuofengzhang/flake/client/controller/DashboardController.java
+++ flake-client/src/main/java/com/github/zuofengzhang/flake/client/controller/DashboardController.java
@@ -2,19 +2,19 @@ package com.github.zuofengzhang.flake.client.controller;
 
 import com.github.zuofengzhang.flake.client.entity.TaskEntity;
 import com.github.zuofengzhang.flake.client.entity.TaskType;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.util.Callback;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.URL;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
