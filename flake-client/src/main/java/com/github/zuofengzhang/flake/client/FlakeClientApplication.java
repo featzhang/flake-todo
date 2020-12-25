f00e467b8a8c623efe542b9054dd2b993a072dea
averyzhang <averyzhang@tencent.com>
2020-12-25 09:25:21 +0800

    build package using spring-boot

--- flake-client/src/main/java/com/github/zuofengzhang/flake/client/FlakeClientApplication.java
+++ flake-client/src/main/java/com/github/zuofengzhang/flake/client/FlakeClientApplication.java
@@ -2,7 +2,6 @@ package com.github.zuofengzhang.flake.client;
 
 import javafx.application.Application;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
